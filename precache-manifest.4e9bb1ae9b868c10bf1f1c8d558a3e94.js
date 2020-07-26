aa08c6a80114191c15fb44a12f01a50aeec91f62
Yilmar Alejandro Muñoz Segovia <yimy@MacBook-Pro-de-Yilmar-2012.local>
2020-07-26 18:04:39 -0400

    Updates

---
+++ precache-manifest.4e9bb1ae9b868c10bf1f1c8d558a3e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f471cc2f7eff92ffd34ac9179548a55d",
+    "url": "/XxyimyxX/react_tutorial_api/index.html"
+  },
+  {
+    "revision": "dd77589a4fa6090d8dd0",
+    "url": "/XxyimyxX/react_tutorial_api/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "aef1d30c00621f6e5651",
+    "url": "/XxyimyxX/react_tutorial_api/static/js/2.da419696.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/XxyimyxX/react_tutorial_api/static/js/2.da419696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd77589a4fa6090d8dd0",
+    "url": "/XxyimyxX/react_tutorial_api/static/js/main.8ad63934.chunk.js"
+  },
+  {
+    "revision": "f810d861d5bc7abb2fd3",
+    "url": "/XxyimyxX/react_tutorial_api/static/js/runtime-main.f2a4321e.js"
+  }
+]);
